da903378aad39d1b7271a8f09c54da2358e9dbfa
Mohamad Kayali <147993209+mohakay73@users.noreply.github.com>
2024-02-20 12:50:36 +0100

    metamask

--- script/metamask.js
+++ script/metamask.js
@@ -1,5 +1,12 @@
-console.log(window.ethereum);
 const connectButton = document.querySelector('#connect');
+const accountInput = document.querySelector('#accountNumber');
+const checkBalanceButton = document.querySelector('#checkBalance');
+const displayBalance = document.querySelector('#balance');
+const sendButton = document.querySelector('#sendTx');
+const toAccountInput = document.querySelector('#toAccountNumber');
+const valueInput = document.querySelector('#amount');
+
+let acccounts;
 
 async function connect() {
   if (typeof window.ethereum != undefined) {
@@ -13,20 +20,17 @@ async function connect() {
   }
 }
 
-connectButton.addEventListener('click', connect);
-
-const accountInput = document.querySelector('#accountNumber');
-const checkBalanceButton = document.querySelector('#checkBalance');
-const displayBalance = document.querySelector('#balance');
-const sendButton = document.querySelector('#sendTx');
-const toAccountInput = document.querySelector('#toAccountNumber');
-const valueInput = document.querySelector('#amount');
-
-let acccounts;
-
 async function checkBalance() {
   if (typeof ethereum !== undefined) {
     acccounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+    const balance = await ethereum.request({
+      method: 'eth_getBalance',
+      params: [accountInput.value, 'latest'],
+    });
+    const parsedBalanced = parseInt(balance) / Math.pow(10, 18);
+    displayBalance.innerText = parsedBalanced;
+    console.log(parsedBalanced);
   } else {
     console.log('No ethereum');
   }
@@ -34,6 +38,21 @@ async function checkBalance() {
 
 async function sendFunds() {
   try {
+    const amount = parseFloat(valueInput.value) * Math.pow(10, 18);
+    let params = [
+      {
+        from: accountInput.value,
+        to: toAccountInput.value,
+        value: Number(amount).toString(16),
+        gas: Number(21000).toString(16),
+        gasPrice: Number(2500000).toString(16),
+      },
+    ];
+
+    const response = await ethereum.request({
+      method: 'eth_sendTransaction',
+      params: params,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -41,3 +60,4 @@ async function sendFunds() {
 
 checkBalanceButton.addEventListener('click', checkBalance);
 sendButton.addEventListener('click', sendFunds);
+connectButton.addEventListener('click', connect);
